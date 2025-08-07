6890f1f5cd06d37311945b293c9f27e9c431884e
harsh589 <harshyadav1457@gmail.com>
2025-08-08 00:09:31 +0530

    2 commit

--- numpyexample.py
+++ numpyexample.py
@@ -21,3 +21,26 @@ arr4 = np.array([1, 2, 3, 4], dtype='S')
 
 print(arr4)
 print(arr4.dtype)
+
+#copy vs view
+
+
+arr5 = ([3,4,5,6,7])
+
+copy_arr5 = arr5.copy()
+
+arr5[0] = 100
+
+print(arr5)
+
+#copy wale mai change nahi hoga
+
+
+#view mai chnage ho jayega sab jagah
+arr2 = np.array([10, 20, 30, 40])
+view_arr = arr2.view()
+
+arr2[1] = 777
+
+print("Original:", arr2)      # [10 777 30 40]
+print("View:    ", view_arr)  # [10 777 30 40]
