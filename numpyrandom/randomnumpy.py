36f71f7e0153c55ab1036e173580c3019cdeb660
harsh589 <harshyadav1457@gmail.com>
2025-08-18 22:16:01 +0530

    numpynew

---
+++ numpyrandom/randomnumpy.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x = np.random.randint(100)   # ✅ 0 se 99 ke beech ek random integer
+print(x)
+
+
--- numpysearch.py
+++ numpysearch.py
@@ -5,3 +5,5 @@ arr = np.array([1, 2, 32, 4, 5, 4, 4])
 x = np.where(arr == 32)
 
 print(x)
+
+
