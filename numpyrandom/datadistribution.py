f0b90d37637be7b0ee6c713c2b2aaffdbb60a2a7
harsh589 <harshyadav1457@gmail.com>
2025-08-24 20:48:30 +0530

    data

---
+++ numpyrandom/datadistribution.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+x = np.random.choice([3,4,5], p=[0.6,0.3,0.1], size=(10))
+print(x);
--- numpyrandom/randomnumpy.py
+++ numpyrandom/randomnumpy.py
@@ -3,4 +3,11 @@ import numpy as np
 x = np.random.randint(100)   # ✅ 0 se 99 ke beech ek random integer
 print(x)
 
+#rand use for flaott and randint use for int
 
+y = np.random.randint(100,size=(7))
+print(y)
+
+
+z = np.random.choice(([3, 5, 7, 9]))
+print(z)
